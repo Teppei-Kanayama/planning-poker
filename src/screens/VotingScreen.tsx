183c41a6413a28c4db10504611d029fe55f0152d
Teppei Kanayama <kanayama.teppei@gmail.com>
2022-03-20 19:39:16 +0900

    define card

--- src/screens/VotingScreen.tsx
+++ src/screens/VotingScreen.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-use-before-define */
-import React from 'react'
+import React, { useState } from 'react'
 import { useSearchParams } from 'react-router-dom'
 import { addVote, deleteVote, deleteAllVotes } from '../firebase/firebase'
 import { v4 as uuidv4 } from 'uuid'
@@ -16,7 +16,16 @@ const getUserId = () => {
   return userId
 }
 
+const VoteCard = ({ point, onClick }: {point: number, onClick: (p: number) => void}) => {
+  return (
+    <button onClick={() => { onClick(point) }}>{point}</button>
+  )
+}
+
+const fibonacci = [0, 1, 2, 3, 5, 8, 13, 21]
+
 export const VotingScreen = () => {
+  const [point, setPoint] = useState<number>()
   const [searchParams] = useSearchParams()
   const roomId = searchParams.get('id')
   const roomSize = searchParams.get('size')
@@ -43,14 +52,30 @@ export const VotingScreen = () => {
     await deleteVote(roomId, userId)
   }
 
+  const onClickVoteCard = (p: number) => {
+    setPoint(p)
+  }
+
   return (
     <>
       <button onClick={onClickResetAllVotes}>
           全員の投票をリセット
       </button>
       <h1>
-        { `あなたのuserIdは${userId}、roomIdは${roomId}、roomSizeは${roomSize}です` }
+        { `あなたのuserIdは${userId}、roomIdは${roomId}、roomSizeは${roomSize}です、pointは${point}です` }
       </h1>
+      {
+        fibonacci.map((i) => {
+          return (
+            <VoteCard
+              key={i}
+              point={i}
+              onClick={onClickVoteCard}
+            />
+          )
+        })
+      }
+      <br />
       <button onClick={onClickVote}>
           投票
       </button>
