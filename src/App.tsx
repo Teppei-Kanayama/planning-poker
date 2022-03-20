a3a413b56e8dcf44b9aceb8ad4efd46edd208766
Teppei Kanayama <kanayama.teppei@gmail.com>
2022-03-20 16:09:20 +0900

    react router works on firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 /* eslint-disable no-use-before-define */
 import React from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { SetupScreen } from './screens/SetupScreen'
 import { VotingScreen } from './screens/VotingScreen'
 
 function App () {
   return (
-    <>
+    <BrowserRouter>
       <Routes>
-        <Route path="/" element={<SetupScreen />} />
         <Route path="/create-new-room" element={<SetupScreen />} />
         <Route path="/room" element={<VotingScreen />} />
+        <Route path="/" element={<SetupScreen />} />
       </Routes>
-    </>
+    </BrowserRouter>
   )
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,20 @@
 /* eslint-disable no-use-before-define */
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
-import { BrowserRouter } from 'react-router-dom'
+
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import { SetupScreen } from './screens/SetupScreen'
+import { VotingScreen } from './screens/VotingScreen'
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Link to="/create-new-room">Invoices</Link>
+    <Link to="/room">Expenses</Link>
+    <Routes>
+      <Route path="/create-new-room" element={<SetupScreen />} />
+      <Route path="/room" element={<VotingScreen />} />
+      <Route path="/" element={<SetupScreen />} />
+    </Routes>
   </BrowserRouter>,
   document.getElementById('root')
 )
