92c0f63c614942a00c86bf1992a527cceb047f9a
Teppei Kanayama <kanayama.teppei@gmail.com>
2022-03-20 17:11:41 +0900

    wip

--- src/screens/SetupScreen.tsx
+++ src/screens/SetupScreen.tsx
@@ -1,13 +1,39 @@
 /* eslint-disable no-use-before-define */
-import React from 'react'
+import React, { ChangeEvent, useState } from 'react'
 
 export const SetupScreen = () => {
-  console.log(location.protocol)
+  const [size, setSize] = useState(0)
+  const [roomUrl, setRoomUrl] = useState<string>()
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setSize(parseInt(event.target.value))
+  }
+
+  const handleClick = () => {
+    setRoomUrl(`${location.protocol}//${location.host}/room?id=100&size=${size}`)
+  }
 
   return (
     <div className="App">
       <h1>新しい部屋を作成しよう！</h1>
-      <a href={`${location.protocol}//${location.host}/room?id=100&size=10`}>部屋に移動する</a>
+
+      <label>
+        参加人数:
+        <input type="number" name="name" onChange={handleChange} />
+      </label>
+
+      <button onClick={handleClick}>作成する</button>
+
+      {
+        roomUrl && (
+          <>
+            <br />
+            <>{roomUrl}</>
+            <br />
+            <a href={roomUrl} target="_blank" rel="noreferrer">部屋に移動する</a>
+          </>
+        )
+      }
     </div>
   )
 }
