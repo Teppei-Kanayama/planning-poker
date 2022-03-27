d9ba3befe5b5bc36da90cc9b5a25e50647384ac9
Teppei Kanayama <kanayama.teppei@gmail.com>
2022-03-27 11:06:49 +0900

    loading screen

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,23 @@ import { Routes, Route, BrowserRouter } from 'react-router-dom'
 
 import { SetupScreen } from './screens/SetupScreen'
 import { VotingScreen } from './screens/VotingScreen'
-import { SignInScreen } from './screens/SignInScreen'
+// import { SignInScreen } from './screens/SignInScreen'
 import { useSignIn } from './hooks/firebase'
+import ReactLoading from 'react-loading'
+
+const style = {
+  marginTop: '20rem',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center'
+}
 
 function App () {
   const [isSignedIn] = useSignIn()
 
   if (!isSignedIn) {
-    return <SignInScreen/>
+    // return <SignInScreen/>
+    return (<div style={style}><ReactLoading type="spokes" color="#0000FF" height={200} width={100} /></div>)
   }
   return (
     <BrowserRouter>
