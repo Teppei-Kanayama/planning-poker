5f3506e68e361dd48bbcbd6bbdbedb9241ca3adb
Teppei Kanayama <kanayama.teppei@gmail.com>
2022-03-27 11:10:54 +0900

    loading state

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,28 @@ import { Routes, Route, BrowserRouter } from 'react-router-dom'
 
 import { SetupScreen } from './screens/SetupScreen'
 import { VotingScreen } from './screens/VotingScreen'
-// import { SignInScreen } from './screens/SignInScreen'
+import { SignInScreen } from './screens/SignInScreen'
 import { useSignIn } from './hooks/firebase'
 import ReactLoading from 'react-loading'
 
-const style = {
-  marginTop: '20rem',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center'
-}
-
 function App () {
-  const [isSignedIn] = useSignIn()
+  const [isSignedIn, isLoading] = useSignIn()
+
+  if (isLoading) {
+    return (
+    <div style={{
+      marginTop: '20rem',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center'
+    }}>
+      <ReactLoading type="spokes" color="#0000FF" height={200} width={100} />
+      </div>
+    )
+  }
 
   if (!isSignedIn) {
-    // return <SignInScreen/>
-    return (<div style={style}><ReactLoading type="spokes" color="#0000FF" height={200} width={100} /></div>)
+    return <SignInScreen/>
   }
   return (
     <BrowserRouter>
--- src/hooks/firebase.ts
+++ src/hooks/firebase.ts
@@ -8,15 +8,16 @@ import { firebaseKeys } from '../data/constants'
 firebase.initializeApp(firebaseKeys)
 
 export const useSignIn = () => {
-  const [isSignedIn, setIsSignedIn] = useState(false) // Local signed-in state.
+  const [isSignedIn, setIsSignedIn] = useState(false)
+  const [isLoading, setIsLoading] = useState(true)
 
-  // Listen to the Firebase Auth state and set the local state.
   useEffect(() => {
     const unregisterAuthObserver = firebase.auth().onAuthStateChanged(user => {
       setIsSignedIn(!!user)
+      setIsLoading(false)
     })
     return () => unregisterAuthObserver() // Make sure we un-register Firebase observers when the component unmounts.
   }, [])
 
-  return [isSignedIn]
+  return [isSignedIn, isLoading]
 }
