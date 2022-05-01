171132ba1e5cbc53d1a0aab92bc770ad02ae761d
Teppei Kanayama <kanayama.teppei@gmail.com>
2022-05-01 20:38:10 +0900

    vote result

--- src/components/VoteResult.tsx
+++ src/components/VoteResult.tsx
@@ -1,5 +1,7 @@
 /* eslint-disable no-use-before-define */
 import React from 'react'
+import { MdAccountCircle } from 'react-icons/md'
+import ReactTooltip from 'react-tooltip'
 import { Vote } from '../types'
 import { VoteCards } from './Cards'
 
@@ -7,6 +9,23 @@ export const VoteResult = ({ votes }: {votes: Vote[]}) => {
   const points = votes.map((vote) => vote.point)
   return (
     <>
+      <div style={{ display: 'flex', marginLeft: '1rem', fontSize: '1.5em' }}>
+        {
+          votes.map((vote) => {
+            const user = vote.user
+            const style = { height: '3rem', marginLeft: '0.5rem', marginRight: '0.5rem' }
+            if (user.iconUrl == null) {
+              return <MdAccountCircle key={user.id} style={style}/>
+            }
+            return (
+              <div key={user.id}>
+              <img src={user.iconUrl} style={style} data-tip={user.name}/>
+              <ReactTooltip />
+              </div>
+            )
+          })
+        }
+      </div>
       <VoteCards points={points} disabled/>
     </>
   )
