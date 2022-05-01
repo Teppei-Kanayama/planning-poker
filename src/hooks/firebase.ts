8334eec9a96bb043842c6d4715c37a93192c0490
Teppei Kanayama <kanayama.teppei@gmail.com>
2022-05-01 17:23:39 +0900

    user object

--- src/hooks/firebase.ts
+++ src/hooks/firebase.ts
@@ -4,24 +4,29 @@ import { useEffect, useState } from 'react'
 import firebase from 'firebase/compat/app'
 import 'firebase/compat/auth'
 import { firebaseKeys } from '../data/constants'
+import { User } from '../types'
 
 firebase.initializeApp(firebaseKeys)
 
 export const useSignIn = () => {
+  const [user, setUser] = useState<User>()
   const [userId, setUserId] = useState<string>()
   const [photoUrl, setPhotoUrl] = useState<string>()
   const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
-    const unregisterAuthObserver = firebase.auth().onAuthStateChanged(user => {
-      if (user != null) {
-        setUserId(user.uid)
-        setPhotoUrl(user.photoURL || '')
+    const unregisterAuthObserver = firebase.auth().onAuthStateChanged(u => {
+      if (u != null) {
+        console.log(u)
+        setUser({ id: u.uid, name: u.displayName, iconUrl: u.photoURL })
+        setUserId(u.uid)
+        setPhotoUrl(u.photoURL || '')
       }
       setIsLoading(false)
     })
     return () => unregisterAuthObserver() // Make sure we un-register Firebase observers when the component unmounts.
   }, [])
 
+  console.log(user)
   return [userId, photoUrl, isLoading] as const
 }
---
+++ src/types.ts
@@ -0,0 +1,5 @@
+export type User = {
+  id: string,
+  iconUrl: string | null,
+  name: string | null,
+}
