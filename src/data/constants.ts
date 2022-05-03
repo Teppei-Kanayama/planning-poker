b2e89d609d035caa0574dbe00b9f10c1c050b380
Teppei Kanayama <kanayama.teppei@gmail.com>
2022-05-03 16:27:58 +0900

    add constants under git

---
+++ src/data/constants.ts
@@ -0,0 +1,7 @@
+// API keys for Firebase services are safe to include in code
+// https://firebase.google.com/docs/projects/api-keys
+export const firebaseKeys = {
+  apiKey: 'AIzaSyCgINdE7XhKYXCCbfcf5DVMYB1L6zqatsc',
+  authDomain: 'planning-poker-56058.firebaseapp.com',
+  projectId: 'planning-poker-56058'
+}
