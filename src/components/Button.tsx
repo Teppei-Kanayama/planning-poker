4fbc4a6f999b47ede81e178fe67d2bf6890dbe53
Teppei Kanayama <kanayama.teppei@gmail.com>
2022-07-02 23:48:18 +0900

    delete button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Button from 'react-bootstrap/Button'
 
 import { useAlertContext } from '../hooks/alert'
-import { signOut, deleteAllVotes } from '../data/firebase'
+import { deleteAllVotes } from '../data/firebase'
 
 export const ResetButton = ({ roomId }: {roomId: string}) => {
   const { setAlert, resetAlert } = useAlertContext()
@@ -15,25 +15,5 @@ export const ResetButton = ({ roomId }: {roomId: string}) => {
     <Button onClick={handleClick} variant='danger' style={{ marginLeft: '1rem', marginBottom: '1rem', display: 'flex' }}>
       全員の投票をリセット
     </Button>
-  )
-}
-
-export const SignOutButton = () => {
-  return (
-    <div style={{ display: 'flex' }}>
-      <Button
-        variant='success'
-        onClick={
-          () => {
-            signOut()
-            location.reload()
-          }
-        }
-      >
-        <div style={{ color: 'white' }}>
-          サインアウト
-        </div>
-      </Button>
-    </div>
   )
 }
