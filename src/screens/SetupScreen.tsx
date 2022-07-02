04aa93f63a883a12592e55caea3965c47525ec0e
Teppei Kanayama <kanayama.teppei@gmail.com>
2022-07-02 18:41:53 +0900

    wip

--- src/screens/SetupScreen.tsx
+++ src/screens/SetupScreen.tsx
@@ -23,11 +23,11 @@ export const SetupScreen = () => {
   return (
     <>
       <SignOutButton />
-      <h1 style={{ justifyContent: 'center', display: 'flex', fontWeight: 'bold', marginBottom: '2rem' }}>新規投票所の作成</h1>
+      <h1 style={{ display: 'flex', justifyContent: 'center', fontWeight: 'bold', marginBottom: '2rem' }}>新規投票所の作成</h1>
 
-      <Form.Group className="mb-3" style={{ marginLeft: '1rem', display: 'flex', alignItems: 'center' }}>
+      <Form.Group className='mb-3' style={{ display: 'flex', alignItems: 'center', marginLeft: '1rem' }}>
         <Form.Label style={{ display: 'flex', alignItems: 'end' }}>参加人数</Form.Label>
-        <Form.Control type="number" onChange={handleChange} style={{ width: '5rem', marginLeft: '1rem', marginRight: '1rem' }}/>
+        <Form.Control type='number' min='1' onChange={handleChange} style={{ width: '5rem', marginLeft: '1rem', marginRight: '1rem' }}/>
             人
         <Button
           onClick={handleClick}
@@ -39,15 +39,15 @@ export const SetupScreen = () => {
       {
         roomUrl && (
           <>
-            <h3 style={{ padding: '1rem' }}>
+            <p>
               ↓投票所が作成されました！投票所のリンクを参加者に配布しましょう。
-            </h3>
+            </p>
 
-            <a href={roomUrl} target="_blank" rel="noreferrer" style={{ padding: '1rem' }}>
+            <a href={roomUrl} target='_blank' rel='noreferrer' style={{ padding: '1rem' }}>
               {roomUrl} <MdOpenInNew size={30}/>
             </a>
             <CopyToClipboard text={roomUrl} onCopy={() => { setCopied(true) }}>
-              <Button variant="light" size='lg' ><MdOutlineContentCopy/></Button>
+              <Button variant='light' size='lg' ><MdOutlineContentCopy/></Button>
             </CopyToClipboard>
             {
               copied && (
